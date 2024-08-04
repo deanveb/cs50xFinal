c1e78ac3b83a92601de5b63d5d3e97662ea94328
deanv <Annhi1975@gmail.com>
2024-08-04 15:51:32 +0700

    login page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+from flask_session import Session
+from sqlite3 import SQL
+
+from helper import login_required
 
 app = Flask("__name__")
+app.run(debug=True)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -8,4 +13,15 @@ Session(app)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"]) 
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        # TODO
+        # Get data
+        # check confirm in js
+        # add to sqlite3
+        return redirect("/")
