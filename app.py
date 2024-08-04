3a5dd9b6d28e39e8d7e84432085fbb316ddd4be2
deanv <Annhi1975@gmail.com>
2024-08-04 14:26:07 +0700

    extra setup

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask
 
-app = Flask()
+app = Flask("__name__")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
